36cf20e2f598f9374102cee0fb141231510ce884
Tejada234 <78103349+Tejada234@users.noreply.github.com>
2022-11-29 09:57:22 +0100

    Add files via upload

---
+++ CRM_Alejandro_Tejada/CRM_Alejandro_Tejada/src/main/java/com/CRM/app/CRM_Alejandro_Tejada/CrmAlejandroTejadaApplication.java
@@ -0,0 +1,13 @@
+package com.CRM.app.CRM_Alejandro_Tejada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAlejandroTejadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmAlejandroTejadaApplication.class, args);
+	}
+
+}
---
+++ CRM_Alejandro_Tejada/CRM_Alejandro_Tejada/src/test/java/com/CRM/app/CRM_Alejandro_Tejada/CrmAlejandroTejadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRM.app.CRM_Alejandro_Tejada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAlejandroTejadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
